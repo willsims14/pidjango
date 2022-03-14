dffe5d5b781788841fdf90c551a1464cd010c0ec
willsims14 <willsims14@gmail.com>
2022-03-14 11:08:54 -0500

    Updated settings

--- pidjango/settings.py
+++ pidjango/settings.py
@@ -36,7 +36,8 @@ ALLOWED_HOSTS = [
 
 # Application definition
 INSTALLED_APPS = [
-    'plants.apps.PlantsConfig',
+    'plants',
+    # 'plants.apps.PlantsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
