3c2967e2507f8cdbd0f1c52ae8713ec7e11040f9
willsims14 <willsims14@gmail.com>
2022-03-20 17:40:31 -0500

    asdf

--- plants/admin.py
+++ plants/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from .models import Plant, Propogation
 
+
+class PropogationAdmin(admin.ModelAdmin):
+    fields = ['plant', 'prop_location']
+
+
 admin.site.register(Plant)
-admin.site.register(Propogation)
+# admin.site.register(Propogation)
+admin.site.register(PropogationAdmin)
