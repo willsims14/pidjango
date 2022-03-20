cb2a56a5c30ad459771803beb22fe3ad89b88d5d
willsims14 <willsims14@gmail.com>
2022-03-20 11:29:45 -0500

    Added plants templates

--- plants/views.py
+++ plants/views.py
@@ -1,3 +1,4 @@
+from dajngo.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Plant
@@ -5,12 +6,18 @@ from .models import Plant
 
 def index(request):
     latest_plant_list = Plant.objects.order_by('-meta_created')[:5]
-    output = ', '.join([p.common_name for p in latest_plant_list])
-    return HttpResponse(output)
+    # output = ', '.join([p.common_name for p in latest_plant_list])
+    # template = loader.get_template('plants/index.html')
+    context = {
+        'latest_plant_list': latest_plant_list,
+    }
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'plants/index.html', context)
 
 
 def detail(request, plant_id):
-    return HttpResponse("You're looking at plant %s." % plant_id)
+    plant = get_object_or_404(Plant, pk=plant_id)
+    return render(request, 'plants/detail.html', {'plant': plant})
 
 
 def propogations(request, plant_id):
