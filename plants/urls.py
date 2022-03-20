d086200cb779306ab99c3533b99aed289068cc85
willsims14 <willsims14@gmail.com>
2022-03-20 13:10:57 -0500

    asd

--- plants/urls.py
+++ plants/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'plants'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),  # /plants/
-    path('<int:plant_id>/', views.DetailView.as_view(), name='detail'),  # /plants/5/
+    path('<int:plant_id>/', views.detail, name='detail'),  # /plants/5/
     # path('<int:plant_id>/propogations/', views.propogations, name='propogations'),  # /plants/5/propogations/
     # path('<int:plant_id>/propogate/', views.propogate, name='propogate'),  # /plants/5/propogate/
 ]
