2f5aac5b76a2c0cb3abc6ff09b88f0c7c88a74c3
willsims14 <willsims14@gmail.com>
2022-03-20 17:47:47 -0500

    asdf

--- plants/urls.py
+++ plants/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-# app_name = 'plants'
+app_name = 'plants'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),  # /plants/
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),  # /plants/5/
