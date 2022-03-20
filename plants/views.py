73ff4aab76f824639974d59507a22e222d75a841
willsims14 <willsims14@gmail.com>
2022-03-20 11:12:06 -0500

    Fixed plants detail view

--- plants/views.py
+++ plants/views.py
@@ -1,20 +1,22 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+
+from .models import Plant
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-meta_created')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
+    latest_plant_list = Plant.objects.order_by('-meta_created')[:5]
+    output = ', '.join([p.common_name for p in latest_plant_list])
     return HttpResponse(output)
+
 
 def detail(request, plant_id):
     return HttpResponse("You're looking at plant %s." % plant_id)
 
+
 def propogations(request, plant_id):
     response = "You're looking at the propogations of plant %s."
     return HttpResponse(response % plant_id)
+
 
 def propogate(request, plant_id):
     return HttpResponse("You're propogating %s." % plant_id)
